d12b268f23a8f9927535221e021569c22465ca59
Stevemcglinchey-creator <Stevemcglinchey@rocketmail.com>
2026-03-23 12:00:47 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+// Simple service worker for PWA
+const CACHE_NAME = 'senwear-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/Icon-192.png',
+  '/Icon-512.png',
+  '/Team_Pilot.mp4'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
