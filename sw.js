f6c54ff7dcf0cda75afe7e26d96915848157277e
Stevemcglinchey-creator <Stevemcglinchey@rocketmail.com>
2026-04-10 08:11:40 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Basic service worker for PWA
-self.addEventListener('install', function(event) {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
-});
